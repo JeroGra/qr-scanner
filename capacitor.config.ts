4c9af464b0e791d5ab569dea81dfb925ae37b98c
Jeronimo Granadillo <Jerog000@gmail.com>
2024-11-08 20:28:47 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Qr_Scanner',
+  webDir: 'www'
+};
+
+export default config;
